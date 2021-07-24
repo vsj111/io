98be6ff2fdb2cd0e4b871c0cedc8e192472fc6ee
vsj111 <84206186+vsj111@users.noreply.github.com>
2021-07-25 00:06:23 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://www.igao12.com', "_self");
+            window.open('http://www.vsj12.com', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://www.igao13.com', "_self");
+            window.open('http://www.vsj13.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://www.igao14.com', "_self");
+            window.open('http://www.vsj14.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://www.igao15.com', "_self");
+            window.open('http://www.vsj15.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
-            window.open('http://www.meizuhui.com/', "_blank");
+            window.open('https://www.baidu.com/', "_blank");
         }
     }
     function init() {
