551b347015cccd39f442ee7226db0074993a20fb
vsj111 <84206186+vsj111@users.noreply.github.com>
2022-08-04 01:56:17 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,16 +1,16 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://vsj001.com', "_self");
+            window.open('http://vsjapp.szlongdiwei.com/?pt', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://vsj001.com', "_self");
+            window.open('http://vsjapp.szlongdiwei.com/?pt', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://vsj001.comm', "_self");
+            window.open('http://vsjapp.szlongdiwei.com/?pt', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://vsj001.com', "_self");
+            window.open('http://vsjapp.szlongdiwei.com/?pt', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('https://www.baidu.com/', "_blank");
